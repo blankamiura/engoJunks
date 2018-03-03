6e64de98fc39f470ac9bdb416633a13c44f516b1
blankamiura <blankamiura@gmail.com>
2018-03-03 13:49:03 +0900

    Inserting comments

--- Game/part0.go
+++ Game/part0.go
@@ -8,23 +8,33 @@ import (
 	"engo.io/engo/common"
 )
 
+// Create a Scene "DefaultScene"
 type DefaultScene struct{}
 
+// Setting DefaultScene's Type
 func (*DefaultScene) Type() string { return "GameWorld" }
 
+// Setting DefaultScene's Preload()
 func (*DefaultScene) Preload() {}
 
+// Setting DefaultScene's Setup()
+// This is executed before entering main loop
 func (*DefaultScene) Setup(w *ecs.World) {
+  // Making RenderSystem registering RenderSystem
+  // This System is neccesary for drawing background here
   w.AddSystem(&common.RenderSystem{})
 
+  // Setting a white background
 	common.SetBackground(color.White)
 }
 
 func main() {
+  // Setting window options as RunOptions
 	opts := engo.RunOptions{
 		Title:  "Hello World",
 		Width:  1024,
 		Height: 640,
 	}
+  // Executing the engo project using DefaultScene
 	engo.Run(opts, &DefaultScene{})
 }
